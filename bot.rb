b750b889a75b4e6ba10ac63b6f095b73341cf5ae
Plazma <none>
2010-08-02 06:21:52 -0700

    first hacks

---
+++ bot.rb
@@ -0,0 +1,21 @@
+require 'socket'
+
+server = "wenduri.darkdna.net"
+port = 6667
+nick = "Plazma-bot"
+channel = "#lobby"
+
+s = TCPSocket.open(server,port)
+s.puts("NICK Plazma-bot")
+s.puts("USER Plazma-bot 8 * :Plaazma")
+s.puts("JOIN #{channel}")
+#s.puts("PRIVMSG #{channel} :MEOW!")
+
+# until we get to the EOF of the socket do stuff
+until s.eof? do
+    if s.gets == "PRIVMSG #{channel} :!beer"
+        s.puts("PRIVMSG #{channel} :Beer for you!")
+    end
+
+end
+
