ea61167b218d4fdbf5241da29584b14b3f304009
Plazma <no@way.org>
2010-08-19 17:27:00 -0500

    Updated to do list , seems to be some bugs to work out

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 require 'MrRoboto-WIP.rb'
 
 #myBot = MrRoboto.new("wenduri.darkdna.net", 6667, "MrRoboto", "#lobby")
-#myBot = MrRoboto.new("chat.freenode.net", 6667, "MrRoboooto", "#botters")
-myBot = MrRoboto.new("localhost", 6667, "MrRoboto", "#bots")
+myBot = MrRoboto.new("chat.freenode.net", 6667, "MrRoboooto", "#botters")
+#myBot = MrRoboto.new("localhost", 6667, "MrRoboto", "#bots")
 
 myBot.connect()
 
