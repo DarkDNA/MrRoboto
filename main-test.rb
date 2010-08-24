234dadc3653166d154c3390d4b4e01d0a05824ff
Plazma <none>
2010-08-24 11:27:39 -0700

    updated git ignore

--- main-test.rb
+++
@@ -1,9 +0,0 @@
-require 'MrRoboto.rb'
-
-#myBot = MrRoboto.new("wenduri.darkdna.net", 6667, "MrRoboto", "#lobby")
-myBot = MrRoboto.new("chat.freenode.net", 6667, "MrRoboooto", "#botters")
-#myBot = MrRoboto.new("localhost", 6667, "MrRoboto", "#bots")
-
-myBot.connect()
-
-myBot.run()
