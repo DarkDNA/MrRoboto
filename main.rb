70a3b7732ab753d60b67dff20e715b5ae9b0da22
Plazma <none>
2010-09-06 23:04:47 -0700

    wee

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require 'MrRoboto.rb'
 
-#myBot = MrRoboto.new("wenduri.darkdna.net", 6667, "MrRoboto", "#bots")
-myBot = MrRoboto.new("chat.freenode.net", 6667, "MrRoboooto", "#botters")
+myBot = MrRoboto.new("wenduri.darkdna.net", 6667, "MrRoboto", "#bots")
+#myBot = MrRoboto.new("chat.freenode.net", 6667, "MrRoboooto", "#botters")
 #myBot = MrRoboto.new("localhost", 6667, "MrRoboto", "#bots")
 
 myBot.connect()
