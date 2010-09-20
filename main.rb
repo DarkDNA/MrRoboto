056e2de4320fcc0f13fd9e13b0118ad910ff06eb
Plazma <none>
2010-09-19 21:27:51 -0700

    corrected readme, updated TODO, added argument # checking

--- main.rb
+++ main.rb
@@ -2,8 +2,15 @@ require './MrRoboto.rb'
 
 # Need to check to make sure we have the correct # of arguments
 
+if ARGV.length != 4
+    
+    puts "Incorrect number of arguments, requies 4"
+    exit
+
+end
 
 # Test  to see if the 2nd argument is a valid number for the port
+
 
 
 # Args are 0 = server, 1 = port, 2 = nick, 3 = starting channel
